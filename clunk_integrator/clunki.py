a1158bad454e88fc629f25d19e0e8537eee689d9
tai <eletai@fasolt4.willab.fi>
2021-08-25 11:07:29 +0300

    First fully working thing

--- clunk_integrator/clunki.py
+++ clunk_integrator/clunki.py
@@ -8,9 +8,11 @@ from PIL import Image, ImageFont, ImageDraw
 
 class Clunki():
     def __init__(self, **kwargs):
-        _service_host = "fasolt4.willab.fi"
-        self._detection_service_url = "http://"+_service_host+":5001/v1/risk_objects/"
-        self._file_storage_url = "https://"+_service_host+":8883/dav/"
+        self.service_host = "fasolt4.willab.fi"
+
+        self.fuseki_db_url = 'http://localhost:8084/fusekiAddImage'
+        self._detection_service_url = "http://"+self.service_host+":5001/v1/risk_objects/"
+        self._file_storage_url = "https://"+self.service_host+":8883/dav/"
         self.dav_options = {
             'webdav_hostname': self._file_storage_url,
             'webdav_login':    "bimuser",
@@ -24,8 +26,64 @@ class Clunki():
         self.webdav_processed_path = 'bimprove-image-storage/processed'
         self.webdav_temp_path = 'bimprove-image-storage/temp'
 
-    def insert_to_risk_db(self, file, fule):
-        pass
+        self.jwt_expiresat = time.time()
+
+    def _auth_with_pydio(self):
+        pydio_auth_req_header = {
+			    'Content-Type': 'application/json',
+			    'Accept': 'application/json',
+			    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
+			}
+
+        client = requests.Session()
+        user = 'bimuser'
+        password = 'bimproveK0ukku55'
+	# Login
+        login_data =  {'AuthInfo': {'login': user, 'password': password, 'type': 'credentials'}}
+        r = client.post("https://"+self.service_host+':8883/a/frontend/session', json=login_data, verify=False, headers=pydio_auth_req_header)
+        # a valid JWT token should be found in the returned JSON object ...
+        jwt_token = r.json()['JWT']
+        jwt_expire = r.json()['ExpireTime']  # in seconds
+        self.jwt_expiresat = int(r.json()['Token']['ExpiresAt'])
+        # Add this to headers
+        self.pydio_auth_headers = {}
+        self.pydio_auth_headers['Content-Type'] = 'application/json'
+        self.pydio_auth_headers['Authorization'] = 'Bearer {}'.format(jwt_token)
+        self.pydio_auth_headers['Cache-Control'] = 'no-cache'
+
+
+    def _find_file_id(self, file_name):
+        if self.jwt_expiresat <= time.time():
+            self._auth_with_pydio()
+        result = requests.get("https://"+self.service_host+":8883/a/tree/stat/bimprove-image-storage/processed/"+file_name, verify=False, headers=self.pydio_auth_headers)
+        trials = 0
+        while result.status_code != 200:
+            result = requests.get("https://"+self.service_host+":8883/a/tree/stat/bimprove-image-storage/processed/"+file_name, verify=False, headers=self.pydio_auth_headers)
+            trials+= 1
+            if trials > 5:
+                break
+        if result.status_code == 200:
+            return result.json()['Node']['Uuid']
+        return None
+
+    def insert_to_risk_db(self, file_name, new_file_name, detections):
+        uuid = self._find_file_id(file_name)
+        if uuid is not None:
+            _header = {'Content-Type': 'text/plain'}
+            for detection in detections:
+                _json_content = {"imageID": uuid,
+                                 "name":detection['name'],
+                                 "confidence": str(detection['confidence']),
+                                 "xmax": str(detection['xmax']),
+                                 "xmin": str(detection['xmin']),
+                                 "ymax": str(detection['ymax']),
+                                 "ymin": str(detection['ymin']),
+                                 "imageURL": "https://fasolt4.willab.fi:8883/public/943468967f2b",
+                                 "anchorBoxImageURL": "https://fasolt4.willab.fi:8883/public/943468967f2b"}
+                print(_json_content)
+                resp = requests.post(self.fuseki_db_url, verify=False, json=_json_content, headers=_header)
+                if resp.status_code != 200:
+                    print(f"Insertion to FUSEKI failed with {resp.status_code}:{resp.text}")
 
     def add_bounding_box_to_img(self, os_file_path, detections):
         img = Image.open(os_file_path)
@@ -43,8 +101,8 @@ class Clunki():
                 draw.text(coords[0], label_text, (0,0,0), font=font)
             new_full_file_name = self.os_temp_file_path + '/bbxes_' + base_file_name
             img.save(new_full_file_name, "JPEG")
-            return new_full_file_name
-        return None
+            return new_full_file_name, detections
+        return None, None
 
     def move_files(self, file_os_path, new_file_os_path):
         #Extract file name from the OS file path
@@ -62,14 +120,18 @@ class Clunki():
             if new_file_name in client.list(self.webdav_temp_path):
                 #client.move(remote_path_from=self.webdav_temp_path+'/'+new_file_name, remote_path_to=self.webdav_processed_path+'/'+new_file_name)
                 client.move(remote_path_from=self.webdav_temp_path+'/'+new_file_name, remote_path_to=self.webdav_processed_path+'/'+new_file_name)
+                return file_name, new_file_name
+        return None, None
 
     def do_inference(self, file_path):
         image_data = open(file_path, "rb").read()
         response = requests.post(self._detection_service_url, files={"image": image_data})
         if len(response.json()) > 0:
-            new_temp_file_and_path = self.add_bounding_box_to_img(file_path, response.json())
+            new_temp_file_and_path, detections = self.add_bounding_box_to_img(file_path, response.json())
             pprint.pprint(response.json())
-            self.move_files(file_path, new_temp_file_and_path)
+            file_name, new_file_name = self.move_files(file_path, new_temp_file_and_path)
+            if file_name is not None and new_file_name is not None and detections is not None:
+                self.insert_to_risk_db(file_name, new_file_name, detections)
 
     def on_created(self, event): # when file is created
         print("Got event for file %s" %event.src_path)
--- clunk_integrator/rest_trial.py
+++ clunk_integrator/rest_trial.py
@@ -1,22 +1,62 @@
 import requests
 import pprint
 
-URL = "https://fasolt4.willab.fi:8883"
+domain = 'https://fasolt4.willab.fi:8883'
+#domain = 'http://192.168.0.114:8080'
+user = 'bimuser'
+password = 'bimproveK0ukku55'
+#user = 'admin'
+#password = 'K0ukku55'
 
-#Get token by docker exec -it a4a93170a819 cells admin user token -u admin -e 1000d
-# -H 'Authorization: Bearer k87prIZsqfMoelu2oS7sis0B08RkHfCTTYmqR1d9WvI.44Vs-_JdMvKqJxf4LwpF7Ry44sxVogTWQGvieSkaBJg' 
-#-H 'Cache-Control: no-cache'
-headers = {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer LHr_zc-yWv_HcWcVEdQqJa99sTZ7RCeZ1eE7zNtkxWM.lcPyrFyD0QQ9LgTt-jTMp0R9BTSJ5ZZoWHxXrGK-gs4',
-            'Cache-Control': 'no-cache',
-          }
+# These will be the same for all requests/responses.
+_headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36'
+}
 
-#response = requests.get(URL+"/a/user/bimuser", verify=False, headers=headers)
+# Create an http session.
+client = requests.Session()
+#client.headers['User-Agent'] = _headers['User-Agent']
 
-response = requests.post(URL+"/a/tree/stats", json={ } ,verify=False, headers=headers)
+# Login
+login_data =  {'AuthInfo': {'login': user, 'password': password, 'type': 'credentials'}}
+r = client.post(domain+'/a/frontend/session', json=login_data, verify=False, headers=_headers)
+
+# a valid JWT token should be found in the returned JSON object ...
+
+jwt_token = r.json()['JWT']
+jwt_expire = r.json()['ExpireTime']  # in seconds
+
+ # Add this to headers
+print(f"This token will expire in {jwt_expire} seconds")
+_headers2 = {}
+_headers2['Content-Type'] = 'application/json'
+_headers2['Authorization'] = 'Bearer {}'.format(jwt_token)
+_headers2['Cache-Control'] = 'no-cache'
+
+#3fe544e7-f485-43e2-b176-1b61a3a5da26
+#'Slug': 'bimprove-image-storage'
+
+#result = requests.post(domain+'/a/tree/stats', json={'NodePaths' : ['bimprove-image-storage/processed/*'] }, verify=False, headers=_headers2)
+
+#result = requests.post(domain+'/a/workspace', json={'Queries' : [{'label':'20-1.jpg'}]}, verify=False, headers=_headers2)
+#result = requests.post(domain+'/a/workspace', json={}, verify=False, headers=_headers2)
+
+#result = requests.post('https://fasolt4.willab.fi:8883/a/share/resources', json={'Limit': 1000}, verify=False, headers=_headers2)
+#pprint.pprint(result.json())
 
 
-pprint.pprint(response.json())
-print(response.request.headers)
-print(response.request.body)
+#result = requests.get(domain+"/a/tree/stat/bimprove-image-storage/processed/bbxes_20-1.jpg", verify=False, headers=_headers2)
+#pprint.pprint(result.json())
+#result = requests.get(domain+"/a/share/cell/e0bdf9ec-3938-4743-8200-87f7aa99eaae", verify=False, headers=_headers2)
+
+#result = requests.get(domain+"/a/share/link/f42d48bd-2ac9-4bbd-b9db-3d046991f7c4", verify=False, headers=_headers2)
+result = requests.get(domain+"/a/share/link/9934c40c-04bf-11ec-8fb0-008cfa000868", verify=False, headers=_headers2)
+
+pprint.pprint(result.json())
+
+#p = { 'ShareLink' : {'Label' : 'Stuff', 'RootNodes' : [{'Uuid' : '50168ba5-be65-43a3-91c4-736956919e8c'}]} }
+#result = requests.put(domain+"/a/share/link", json=p, verify=False, headers=_headers2)
+#print(result)
+#pprint.pprint(result.text)
