a9d8efd7318b3402d9e22e4cdbd155a65e878dc8
Tommi Aihkisalo <tommi.aihkisalo@vtt.fi>
2022-02-09 08:30:18 +0000

    Update pydio_start.sh

--- pydio_start.sh
+++ pydio_start.sh
@@ -1,6 +1,3 @@
-#docker run -d -e CELLS_EXTERNAL=193.166.161.173:1883 -e CELLS_BIND=193.166.161.173:1883 -p 1883:1883 -v /var/cells/ --network=host pydio/cells
-#docker run -d -e CELLS_EXTERNAL=193.166.161.173:8883 -e CELLS_BIND=193.166.161.173:8883 -p 8883:8883 -v /var/cells/ --network=host pydio/cellsll
-#docker run -d -e CELLS_NO_TLS=0 -e CELLS_EXTERNAL=fasolt4.willab.fi:8883 -e CELLS_BIND=fasolt4.willab.fi:8883 -p 8883:8883 -v /var/cells --network=host
-#docker run -d -e CELLS_NO_TLS=0 -e CELLS_EXTERNAL=fasolt4.willab.fi:8883 -e CELLS_BIND=0.0.0.0:8883 -p 8883:8883 -v /var/cells --network=host pydio/cells
-#docker run -d -e CELLS_EXTERNAL=fasolt4.willab.fi:8883 -e CELLS_BIND=0.0.0.0:8883 -p 8883:8883 -v /var/cells:/var/cells/data --network=host pydio/cells
-docker run -d -e CELLS_EXTERNAL=fasolt4.willab.fi:8883 -e CELLS_BIND=0.0.0.0:8883 -p 8883:8883 -v /var/cells/:/var/cells/ --network=host pydio/cells
+#If pydio installed - here is a startup script
+#fasolt4.willab.fi:8883
+docker run -d -e CELLS_EXTERNAL=<External address and port> -e CELLS_BIND=0.0.0.0:8883 -p 8883:8883 -v /var/cells/:/var/cells/ --network=host pydio/cells
